812e410dcd3e04b51bef2244369d872943e7e222
omarcospo <marcos.felipe@usp.br>
2024-07-08 21:17:56 -0300

    add more linters and formatters

--- .config/nvim/lua/plugins/linting.lua
+++ .config/nvim/lua/plugins/linting.lua
@@ -1,19 +1,20 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lint = require("lint")
-    lint.linters_by_ft = {
-      sh = { "shellcheck" },
-      python = { "ruff" },
-      html = { "htmlhint" },
-    }
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "InsertEnter" }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
-  end,
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
+		lint.linters_by_ft = {
+			sh = { "shellcheck" },
+			python = { "ruff" },
+			html = { "htmlhint" },
+			go = { "revive" },
+		}
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "InsertEnter" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
 }
