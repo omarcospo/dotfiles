239bbfbb6f720b3ab602d696f87e6e9628bb2d36
omarcospo <marcos.felipe@usp.br>
2024-07-13 23:20:28 -0300

    fix mpv black artifacts

--- .config/nvim/lua/plugins/markdownish.lua
+++ .config/nvim/lua/plugins/markdownish.lua
@@ -1,17 +1,29 @@
 return {
-  "MeanderingProgrammer/markdown.nvim",
-  name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-  ft = "markdown",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  config = function()
-    require("render-markdown").setup({
-      file_types = { "markdown" },
-      headings = { "󰉫 ", "󰉬 ", "󰉭 ", "󰉮 ", "󰉯 ", "󰉰 " },
-      highlights = {
-        heading = { backgrounds = "Normal" },
-        bullet = "Normal",
-        table = { head = "Normal" },
-      },
-    })
-  end,
+	{
+		"MeanderingProgrammer/markdown.nvim",
+		name = "render-markdown",
+		ft = "markdown",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("render-markdown").setup({
+				file_types = { "markdown" },
+				headings = { "󰉫 ", "󰉬 ", "󰉭 ", "󰉮 ", "󰉯 ", "󰉰 " },
+				highlights = {
+					heading = { backgrounds = "Normal" },
+					bullet = "Normal",
+					table = { head = "Normal" },
+				},
+			})
+		end,
+	},
+	{
+		"jmbuhr/otter.nvim",
+		ft = "markdown",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {},
+		config = function()
+			require("otter").setup({ set_filetype = true })
+			vim.g.markdown_fenced_languages = { "python" }
+		end,
+	},
 }
