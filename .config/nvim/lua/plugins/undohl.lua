20e7d91fda88189056dfe63b40bb65acf1dcda70
omarcospo <marcos.felipe@usp.br>
2024-07-25 01:18:17 -0300

    update

--- .config/nvim/lua/plugins/undohl.lua
+++ .config/nvim/lua/plugins/undohl.lua
@@ -1,23 +1,22 @@
 return {
-  "tzachar/highlight-undo.nvim",
-  event = "User BaseDefered",
-  opts = {
-    hlgroup = "CurSearch",
-    duration = 150,
-    keymaps = {
-      { "n", "u", "undo", {} }, -- If you remap undo/redo, change this
-      { "n", "<C-r>", "redo", {} },
-    },
-  },
-  config = function(_, opts)
-    require("highlight-undo").setup(opts)
-    -- Also flash on yank.
-    vim.api.nvim_create_autocmd("TextYankPost", {
-      desc = "Highlight yanked text",
-      pattern = "*",
-      callback = function()
-        vim.highlight.on_yank()
-      end,
-    })
-  end,
+	"tzachar/highlight-undo.nvim",
+	event = "User BaseDefered",
+	opts = {
+		hlgroup = "CurSearch",
+		duration = 150,
+		keymaps = {
+			{ "n", "u", "undo", {} },
+			{ "n", "<C-r>", "redo", {} },
+		},
+	},
+	config = function(_, opts)
+		require("highlight-undo").setup(opts)
+		vim.api.nvim_create_autocmd("TextYankPost", {
+			desc = "Highlight yanked text",
+			pattern = "*",
+			callback = function()
+				vim.highlight.on_yank()
+			end,
+		})
+	end,
 }
