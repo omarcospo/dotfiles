7f63dd9c596559a11def93876bfbec3dc634e420
omarcospo <marcos.felipe@usp.br>
2024-07-25 23:53:17 -0300

    change otter to sniprun

--- .config/nvim/lua/plugins/markdownish.lua
+++ .config/nvim/lua/plugins/markdownish.lua
@@ -16,14 +16,4 @@ return {
 			})
 		end,
 	},
-	{
-		"jmbuhr/otter.nvim",
-		ft = "markdown",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		opts = {},
-		config = function()
-			require("otter").setup({ set_filetype = true })
-			vim.g.markdown_fenced_languages = { "python" }
-		end,
-	},
 }
