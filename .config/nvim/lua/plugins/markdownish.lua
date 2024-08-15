9e1c0d4b985afda1330d6efa736ed9ae8db8f40b
omarcospo <marcos.felipe@usp.br>
2024-08-15 01:15:54 -0300

    update

--- .config/nvim/lua/plugins/markdownish.lua
+++ .config/nvim/lua/plugins/markdownish.lua
@@ -1,19 +1,27 @@
 return {
+	-- {
+	-- 	"MeanderingProgrammer/markdown.nvim",
+	-- 	name = "render-markdown",
+	-- 	ft = "markdown",
+	-- 	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	-- 	config = function()
+	-- 		require("render-markdown").setup({
+	-- 			file_types = { "markdown" },
+	-- 			headings = { "󰉫 ", "󰉬 ", "󰉭 ", "󰉮 ", "󰉯 ", "󰉰 " },
+	-- 			highlights = {
+	-- 				heading = { backgrounds = "Normal" },
+	-- 				bullet = "Normal",
+	-- 				table = { head = "Normal" },
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 	{
-		"MeanderingProgrammer/markdown.nvim",
-		name = "render-markdown",
-		ft = "markdown",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		config = function()
-			require("render-markdown").setup({
-				file_types = { "markdown" },
-				headings = { "󰉫 ", "󰉬 ", "󰉭 ", "󰉮 ", "󰉯 ", "󰉰 " },
-				highlights = {
-					heading = { backgrounds = "Normal" },
-					bullet = "Normal",
-					table = { head = "Normal" },
-				},
-			})
-		end,
+		"OXY2DEV/markview.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
 	},
 }
