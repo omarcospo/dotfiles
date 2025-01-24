ccb74ac7e5fd16adddd15e3a04ca76523df55153
omarcospo <marcos.felipe@usp.br>
2025-01-24 13:32:29 -0300

    fix python runner problems with indentation in nvim

--- .config/nvim/lua/plugins/runner.lua
+++ .config/nvim/lua/plugins/runner.lua
@@ -5,39 +5,35 @@ return {
 		config = function()
 			local iron = require("iron.core")
 			local view = require("iron.view")
-
 			iron.setup({
 				config = {
 					scratch_repl = true,
 					repl_definition = {
 						sh = { command = { "zsh" } },
 						python = {
-							command = { vim.g.python3_host_prog },
-							format = require("iron.fts.common").bracketed_paste_python,
+							command = { vim.g.python3_host_prog, "-q" },
+							format = require("iron.fts.common").bracketed_paste,
+							block_deviders = { "# %%", "#%%" },
 						},
 					},
 					repl_open_cmd = view.split.vertical.botright("40%"),
 				},
 				keymaps = {
 					send_motion = "<localleader>sc",
-					visual_send = "<localleader>sc",
 					send_file = "<localleader>sf",
 					send_line = "<localleader>sl",
 					send_paragraph = "<localleader>sp",
-					send_until_cursor = "<localleader>su",
 					send_mark = "<localleader>sm",
 					mark_motion = "<localleader>mc",
 					mark_visual = "<localleader>mc",
 					remove_mark = "<localleader>md",
-					cr = "<localleader>s<cr>",
-					interrupt = "<localleader>s<localleader>",
 					exit = "<localleader>sq",
-					clear = "<localleader>cl",
+					clear = "<localleader>cc",
 				},
 				highlight = {
 					italic = true,
 				},
-				ignore_blank_lines = true,
+				ignore_blank_lines = false,
 			})
 			vim.keymap.set("n", "<localleader>rs", "<cmd>IronRepl<cr>")
 			vim.keymap.set("n", "<localleader>rr", "<cmd>IronRestart<cr>")
